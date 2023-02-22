c90a245bd36aab71ff0a24f814fc4ff53554a915
Alexander Thoren <alct@itu.dk>
2023-02-22 18:31:46 +0100

    ✨ Solved ALDAS Week 04 Mandatory Exercise properly

--- ALDAS/4/grades.py
+++ ALDAS/4/grades.py
@@ -27,8 +27,61 @@ for i in range(len(students)):
                 baseval += 1
     students[i] = (name, baseval)
 
-sorted_students = sorted(students, key=lambda vertex: (vertex[1], vertex[0]))
-[print(name) for name, grade in sorted_students]
+def mergesort(a, fn=lambda x: (x, x)):
+    if len(a) < 2:
+        return a
+    hi = len(a)
+    mi = hi // 2
 
-sort(sorted_students)
-print(sorted_students)
+    L = mergesort(a[0:mi], fn)
+    R = mergesort(a[mi:hi], fn)
+    mrgd = merge(L, R, fn)
+    return mrgd
+
+
+def merge(L, R, fn):
+    n1, n2 = len(L), len(R)
+	# Merge the temp arrays back into arr[l..r]
+    i = 0	 # Initial index of first subarray
+    j = 0	 # Initial index of second subarray
+
+    new = []
+    # Look at smallest in each array and add it to new
+    while i < n1 and j < n2:
+        a = L[i]
+        b = R[j]
+
+
+        if fn(a)[0] < fn(b)[0]:
+            new.append(a)
+            i += 1
+        elif fn(a)[0] > fn(b)[0]:
+            j += 1
+            new.append(b)
+        else:
+            # equal, secondary sort
+            if fn(a)[1] <= fn(b)[1]:
+                new.append(a)
+                i += 1
+            elif fn(a)[1] > fn(b)[1]:
+                j += 1
+                new.append(b)
+
+    if i < n1:
+        new.extend(L[i:n1])
+    if j < n2:
+        # print(j, n1, R, R[j:n1])
+        new.extend(R[j:n2])
+        
+    return new
+
+# a = [(0, 1), (0, 0), (0, -1), (0, -2), (0, -3), (0, -4), (0, 1)]
+# print(a)
+# mergesort(a, lambda x: (x[0], x[1]))
+    
+
+# sorted_students = sorted(students, key=lambda vertex: (vertex[1], vertex[0]))
+sorted_students = mergesort(students, fn=lambda x: (x[1], x[0]))
+# print(students)
+# print(sorted_students)
+[print(name) for name, grade in sorted_students]
