757833f1ce6a3df46fe3015a6d134913ec55495c
TheColorman <thoren.alex@gmail.com>
2023-09-27 15:35:50 +0200

    no more please

--- INDBS/4/W04.session.sql
+++ INDBS/4/W04.session.sql
@@ -58,19 +58,22 @@ FOR EACH ROW EXECUTE PROCEDURE CheckBills();
 
 -- 3. Create a trigger on the AccountRecords table that does the following for each insertion: a) checks whether the amount is available (taking the overdraft into account) on the account being withdrawn from, b) updates the balance on the account and c) installs the correct balance into the AccountRecords entry.
 -- Optional: Forbid DELETE and UPDATE operations. This is reflected in the test script.
-CREATE FUNCTION ValidateAccoundRecord()
+CREATE OR REPLACE FUNCTION ValidateAccoundRecord()
 RETURNS TRIGGER
-AS $$ BEGIN
+AS $$
+DECLARE
+    abal INTEGER;
+    aov INTEGER;
+    newbal INTEGER;
+BEGIN
     IF (TG_OP = 'INSERT') THEN
-        DECLARE abal INTEGER;
-        DECLARE aov INTEGER;
-        DECLARE newbal INTEGER;
         BEGIN
-            SELECT abalance, aover, abalance + NEW.ramount INTO abal, aov, newbal
+            SELECT abalance, aover INTO abal, aov
             FROM accounts
             WHERE accounts.aid = NEW.aid;
         END;
-        IF (-NEW.ramount > abal + aov) THEN
+        newbal = abal + NEW.ramount;
+        IF (newbal < -aov) THEN
             RAISE EXCEPTION
                 'you poor 💀 homeless ass 💸'
             USING ERRCODE = '44999';
@@ -96,7 +99,34 @@ AS $$ BEGIN
     END IF;
     RETURN NEW;
 END; $$ LANGUAGE plpgsql;
-CREATE TRIGGER ValidateAccoundRecord
+CREATE OR REPLACE TRIGGER ValidateAccoundRecord
 BEFORE INSERT OR DELETE OR UPDATE
 ON accountrecords
 FOR EACH ROW EXECUTE PROCEDURE ValidateAccoundRecord();
+
+-- 4. Create a procedure Transfer, which takes three parameters, iToAID, iFromAID, and iAmount, and transfers the given amount between the two given accounts in a single transaction. If the amount is not available in the iFromAID  account,  then  the  operation  should  be  aborted  (this  should happen via the trigger from #3). No return value. Optional:  Ensure  that  the  transfer  is  non-negative.  This  is  reflected  in  the test script.
+CREATE OR REPLACE FUNCTION Transfer(
+    iToAID INT,
+    iFromAID INT,
+    iAmount INT
+)
+RETURNS void AS
+$$
+BEGIN
+    INSERT INTO accountrecords (aid, rdate, rtype, ramount)
+    VALUES (iFromAID, NOW(), 'T', -iAmount);
+    INSERT INTO accountrecords (aid, rdate, rtype, ramount)
+    VALUES (iToAID, NOW(), 'T', iAmount);
+RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+-- 5. Create a view DebtorStatus that shows, for each person whose total balance is less than 0, the PID and pName of the person, the total  balance of all their accounts, and the total overdraft of all their accounts.
+-- CREATE VIEW DebtorStatus(
+--     PID, pName, pBalance, pOverdraft
+-- )
+-- AS 
+-- SELECT people.pid, pname, SUM(abalance), SUM(aover) FROM people
+-- JOIN accounts ON accounts.pid = people.pid
+-- GROUP BY people.pid
+-- HAVING SUM(abalance) < 0;
