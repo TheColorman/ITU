b4c3e4fb213ae988d30c0afd24584ca63d5d620b
TheColorman <github@colorman.me>
2025-02-13 22:23:23 +0100

    fix ehe

--- ALPRS/Week01/Server/solution.py
+++ ALPRS/Week01/Server/solution.py
@@ -2,9 +2,10 @@ n, T = [int(i) for i in input().split()]
 ts = [int(i) for i in input().split()]
 
 s = 0
-for i in range(len(ts)):
+for i in range(n):
     t = ts[i]
     s += t
     if s > T:
         print(i)
         exit()
+print(n)
